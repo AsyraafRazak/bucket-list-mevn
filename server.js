560c1d8afacaa40a89cfeb3ed01520f33defcddb
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-10-23 15:08:25 +0800

    end

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const bucketListItemRoutes = require('./routes/api/bucketListItems')
+const path = require('path')
 
 app.use(cors())
 app.use(morgan('tiny'))
@@ -22,6 +23,12 @@ mongoose
     .catch((err) => console.log(err))
 
 app.use('/api/bucketListItems', bucketListItemRoutes)
-app.get('/', (req, res) => res.send('Hello world'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
